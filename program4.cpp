148b14a78b483895e697d0d85daf2334d40e2522
Drashti Maheshwari <drashtiimaheshwari25@gmail.com>
2025-08-12 19:32:30 +0530

    Day1-Calculator of two numbers

---
+++ program4.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+int a , b ;
+cout << "Enter a : ";
+cin >> a ;
+
+cout << "Enter b : ";
+cin >> b ;
+
+int sum = a + b ;
+cout << "Sum = " << sum << endl ;
+
+int difference = a - b ;
+cout << "Difference = " << difference << endl ;
+
+int product = a * b ;
+cout << "Product = " << product << endl ;
+
+int division = a / b ;
+cout << "Division = " << division <<endl ;
+
+int modulo = a % b ;
+cout << "Modulo = " << modulo << endl ;
+  return 0;
+}
